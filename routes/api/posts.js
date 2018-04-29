e0b9f2914d8f0f046ecba6e71e7b31ea8c33b7db
Iam Slaughter <jlgslaughter@gmail.com>
2018-04-29 11:12:51 -0400

    post back end logic done

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Load model
+const Post = require('../../models/Post');
+
+//Load Validation
+const validatePostInput = require('../../validation/post');
 
 //@route    GET api/posts/test
 //@desc     Tests posts route
@@ -7,5 +15,30 @@ const router = express.Router();
 router.get('/test', (req, res) => {
     res.json({msg: 'post works'})
 })
+
+//@route    POST api/posts
+//@desc     Create Post
+//@acess    Private
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+    if (!isValid) {
+        // return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+    })
+    newPost.save()
+    .then(post => {
+        res.json(post);
+    })
+    .catch(err => console.log(err))
+});
 
 module.exports = router;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const passport = require('passport');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
+//Load validation
 const validateProfileInput = require('../../validation/profile');
 const validateExperienceInput = require('../../validation/experience');
 const validateEducationInput = require('../../validation/education');
@@ -98,7 +99,7 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
     const { errors, isValid } = validateProfileInput(req.body);
 
     //check validation
-    if(!isValid){
+    if (!isValid) {
         // return any errors with 400 status
         return res.status(400).json(errors);
     }
