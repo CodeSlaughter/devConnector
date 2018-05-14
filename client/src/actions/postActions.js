8730a0ac0c3577dfd5d5b8183b5d759bb1baabc6
Iam Slaughter <jlgslaughter@gmail.com>
2018-05-14 18:25:15 -0400

    post item component done

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -2,8 +2,18 @@ import axios from 'axios';
 
 import { 
     ADD_POST,
-    GET_ERRORS
+    GET_POSTS,
+    GET_ERRORS,
+    POST_LOADING
 } from './types'
+
+//Set loading state
+export const setPostLoading = () => {
+    return {
+        type: POST_LOADING,
+        payload: true
+    }
+}
 
 //Add post
 export const addPost = postData => dispatch => {
@@ -16,5 +26,20 @@ export const addPost = postData => dispatch => {
         .catch(er => dispatch({
             type: GET_ERRORS,
             payload: er.response.data
+        }))
+}
+
+//Get posts
+export const getPosts = () => dispatch => {
+    dispatch(setPostLoading());
+    axios
+        .get('/api/posts')
+        .then(res => dispatch({
+            type: GET_POSTS,
+            payload: res.data
+        }))
+        .catch(er => dispatch({
+            type: GET_POSTS,
+            payload: null
         }))
 }
