9adf45e54bc4b6d9ff0d1803fd4de0ac02ab52bb
Iam Slaughter <jlgslaughter@gmail.com>
2018-05-16 16:10:34 -0400

    wip

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -6,40 +6,41 @@ import { getProfiles } from '../../actions/profileActions';
 import ProfileItem from './ProfileItem'
 
 class Profiles extends Component {
-    componentDidMount(){
+    componentDidMount() {
         this.props.getProfiles();
     }
-  render() {
-      const { profiles, loading } = this.props.profile;
-      let profileItems;
 
-      if (profiles === null || loading) {
-          profileItems = <Spinner />
-      } else {
-          if (profiles.length > 0) {
-            profileItems = profiles.map((profile) => (
-                <ProfileItem key={profile._id} profile={profile} />
-            ))
-          } else {
-              profileItems = <h4>No profiles found ...</h4>
-          }
-      }
-    return (
-      <div className="profiles">
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <h1 className="display-4 text-center"></h1>
-                    <p className="lead text-center">
-                        Browse and connect with developers
+    render() {
+        const { profiles, loading } = this.props.profile;
+        let profileItems;
+
+        if (profiles === null || loading) {
+            profileItems = <Spinner />
+        } else {
+            if (profiles.length > 0) {
+                profileItems = profiles.map((profile) => (
+                    <ProfileItem key={profile._id} profile={profile} />
+                ))
+            } else {
+                profileItems = <h4>No profiles found ...</h4>
+            }
+        }
+        return (
+            <div className="profiles">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h1 className="display-4 text-center"></h1>
+                            <p className="lead text-center">
+                                Browse and connect with developers
                     </p>
-                    {profileItems}
+                            {profileItems}
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 Profiles.propTypes = {
